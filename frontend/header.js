5eaf62825ac2e03de61b751e098e8f4217b685ad
ShankYadav <shashankyadav29384756@gmail.com>
2023-01-23 12:20:27 +0530

    feat(Product-page-and-search): implemented product page and search using unbxd api

--- frontend/header.js
+++ frontend/header.js
@@ -2,15 +2,20 @@ function getQuery(){
     let searchQuery = document.getElementById("query").value
     console.log(searchQuery)
     window.parent.location=`index.html?q=${searchQuery}`;
-    fetch(`http://127.0.0.1:6969/product-query?q=${searchQuery}`,{
-      method : 'GET',
-      mode :'cors',
-      headers:{
-        'Access-Control-Allow-Origin':'*',
-        'Accept': 'application/json',
-        'Content-Type': 'application/json;charset=utf-8',
-        
-      }
-    }).then(response => response.json()).then(data => console.log(data)).then(console.log("Done"));
   }
     
+  function getCategory1(){
+    let selector = document.querySelector("#cat1");
+    let cat1 = selector.name;
+    let cat2 = selector.value;
+    console.log(cat1);
+    console.log(cat2);
+  }
+
+  function getCategory2(){
+    let selector = document.querySelector("#cat2");
+    let cat1 = selector.name;
+    let cat2 = selector.value;
+    console.log(cat1);
+    console.log(cat2);
+  }

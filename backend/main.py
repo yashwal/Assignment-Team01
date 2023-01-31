0a83be459a388c0f179de68e08699291f2708c3e
ShankYadav <shashankyadav29384756@gmail.com>
2023-01-31 12:37:34 +0530

    feat(Category-sort-and-search-sort-with-pagination): Implemented category and search sort along with pagination for both search and categories

--- backend/main.py
+++ backend/main.py
@@ -61,9 +61,14 @@ class productQuery(Resource):
     def get(self):
         searchQuery = request.args.get('q', default="", type=str)
         pageNumber = request.args.get('page', default=1, type=int)
+        sortKey = request.args.get('sort', default="ftrd", type=str)
+        print(searchQuery,pageNumber,sortKey)
         pageNumber=(pageNumber-1)*9
-        print(pageNumber)
-        response=searchProduct(searchQuery,pageNumber)
+        if sortKey=='ftrd':
+            response = searchProduct(searchQuery,pageNumber,"")
+        else:
+            response=searchSorted(searchQuery,sortKey,pageNumber)
+        #response=searchProduct(searchQuery,pageNumber,sortKey)
         data = json.loads(response.content)
         products = data['response']['products']
         productsNumber = data['response']['numberOfProducts']
@@ -78,11 +83,20 @@ class category(Resource):
         catLevel1 = request.args.get('cat1', default="", type=str)
         catLevel2 = request.args.get('cat2', default="", type=str)
         pageNumber = request.args.get('page', default=1, type=int)
+        sortKey = request.args.get('sort', default="ftrd", type =str)
         pageNumber=(pageNumber-1)*9
         data=searchProducts(catLevel1,catLevel2)
+        #print(data)
         new_data = []
         for product in data:
             new_data.append({"uniqueId":product[0], "Title":product[1].capitalize(), "Description":product[2],"Img_URL":product[3],"price":product[4]})
+        
+        if(sortKey=="price asc"):
+            new_data = sorted(new_data, key=lambda k: float(k['price']))
+        if(sortKey=="price desc"):
+            new_data = sorted(new_data, key=lambda k: float(k['price']), reverse=True)
+        
+        
         sliced_data=new_data[pageNumber:pageNumber+9]
         return [len(new_data),sliced_data]
 
@@ -90,16 +104,15 @@ api.add_resource(category,"/category")
 
 class productSort(Resource):
     def get(self,searchQuery,sortKey,pageNumber):
-        pageNumber=(pageNumber-1)*9
+        pageNumber=max((pageNumber-1)*9,1)
         if sortKey=='ftrd':
-            response = searchProduct(searchQuery,pageNumber)
+            response = searchProduct(searchQuery,pageNumber,"")
         else:
-            response=searchSorted(searchQuery,sortKey)
+            response=searchSorted(searchQuery,sortKey,pageNumber)
         data = json.loads(response.content)
         products = data['response']['products']
         productsNumber = data['response']['numberOfProducts']
-        totalPages = math.ceil(productsNumber/9)
-        return [totalPages,products]
+        return [productsNumber,products]
 
 api.add_resource(productSort,"/product_query/<string:searchQuery>/<string:sortKey>/<int:pageNumber>")
 
@@ -107,21 +120,23 @@ class categorySort(Resource):
     def get(self): #page number should also be an argument
         catLevel1 = request.args.get('cat1', default="", type=str)
         catLevel2 = request.args.get('cat2', default="", type=str)
-        sortKey = request.args.get('sortKey', default="ftrd", type =str)
+        sortKey = request.args.get('sort', default="ftrd", type =str)
         pageNumber = request.args.get('page', default=1, type=int)
         pageNumber=(pageNumber-1)*9
+        print(sortKey)
         data=searchProducts(catLevel1,catLevel2)
-        
+        type(data)
         new_data=[]
         for product in data:
             new_data.append({"uniqueId":product[0], "Title":product[1].capitalize(), "Description":product[2],"Img_URL":product[3],"price":product[4]})
         
         if(sortKey=="price asc"):
-            new_data = sorted(new_data, key=lambda k: k['price'])
-        elif(sortKey=="price desc"):
-            new_data = sorted(new_data, key=lambda k: k['price'], reverse=True)
+            new_data = sorted(new_data, key=lambda k: float(k['price']))
+        if(sortKey=="price desc"):
+            new_data = sorted(new_data, key=lambda k: float(k['price']), reverse=True)
 
         sliced_data=new_data[pageNumber:pageNumber+9]
+
         return [len(new_data),sliced_data]
 
 api.add_resource(categorySort,"/categorySort")
--- frontend/header.js
+++ frontend/header.js
@@ -5,10 +5,12 @@ function getQuery(){
 }
 
 function home(){
-  window.parent.location=`index.html?q=${'*'}`;
+  window.parent.location=`index.html?q=*`;
 }
   
 function getCategory(cat1){
+  
+  
   let selector;
   let cat2;
   if (cat1=='0'){
@@ -25,7 +27,5 @@ function getCategory(cat1){
     cat1 = '2'
     cat2 = "Others"
   }
-  console.log(cat1);
-  console.log(cat2);
-  window.parent.location=`index.html?cat1=${cat1}&cat2=${cat2}`
+  window.parent.location=`index.html?cat1=${cat1}&cat2=${cat2}&page=1&sort=`;
 }

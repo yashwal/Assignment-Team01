72c420a838075793cc0439b4d7f922602a056d2e
Yashwal S Kanchan <kanchanyashwal@gmail.com>
2023-02-01 10:51:29 +0530

    feat(Docker-Deployment-and-Change-in-DB-Schema): Both frontend and backend is deployed on then docker. Database has been changed to new schema which supports category tree and subsequent change in backend is made

--- backend/dataRetrieval.py
+++ backend/dataRetrieval.py
@@ -33,9 +33,7 @@ def searchProducts(catLevel1,catLevel2):
     res=connectDB()
     conn=res[0]
     cur=res[1]
-    
-    cur.execute("select product.product_id,title,description,image_url,price from product FULL OUTER JOIN category02 on product.product_id = category02.product_id where parent_id=%s AND cat_label=%s",(catLevel1,catLevel2))
- 
+    cur.execute("select product_id,title,description,image_url,price from product FULL OUTER JOIN category on product.cat_id = category.cat_id where parent_id=%s AND cat_label=%s ",(catLevel1,catLevel2))
     data=cur.fetchall()
     cur.close()
     conn.close()
--- backend/dataUpdation.py
+++ backend/dataUpdation.py
@@ -5,7 +5,7 @@ def checkProductID(productId):
     res=connectDB()
     conn=res[0]
     cur=res[1]
-    cur.execute("select * from product where product_id=%s",(productId,))
+    cur.execute("select * from product02 where product_id=%s",(productId,))
     data= cur.fetchone()
     cur.close()
     conn.close()
@@ -19,7 +19,7 @@ def updateDB(newTitle,newDesc,newUrl,newPrice,productId):
     res=connectDB()
     conn=res[0]
     cur=res[1]
-    cur.execute("update product set title = %s,description = %s,image_url = %s,price = %s where product_id=%s",(str(newTitle),str(newDesc),str(newUrl),newPrice,productId,))
+    cur.execute("update product02 set title = %s,description = %s,image_url = %s,price = %s where product_id=%s",(str(newTitle),str(newDesc),str(newUrl),newPrice,productId,))
     conn.commit()
     cur.close()
     conn.close()
@@ -32,32 +32,3 @@ def updateDB(newTitle,newDesc,newUrl,newPrice,productId):
 
 
 
-'''
-#future work
-def updateTitle(newTitle,productId):
-    cur.execute("update product set title = %s where product_id=%s",(str(newTitle),productId,))
-    conn.commit()
-    print({"Message":"Title updated"})
-    return 1
-
-def updateDesc(newDesc,productId):
-    cur.execute("update product set decription = %s where product_id=%s",(str(newDesc),productId,))
-    conn.commit()
-    print({"Message":"Description updated"})
-    return 1
-
-
-def updateUrl(newUrl,productId):
-    cur.execute("update product set image_url = %s where product_id=%s",(str(newUrl),productId,))
-    conn.commit()
-    print({"Message":"URL updated"})
-    return 1
-
-
-def updatePrice(newPrice,productId):
-    cur.execute("update product set price = %s where product_id=%s",(str(newPrice),productId,))
-    conn.commit()
-    print({"Message":"Price updated"})
-    return 1
-
-'''

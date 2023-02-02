7cb18deaa8b20bc9a9bbbe2e983819ba5d766952
ShankYadav <shashankyadav29384756@gmail.com>
2023-02-02 14:51:53 +0530

    refactor(Changed-the-css-color-code.): Changed the color schema of the frontend UI

--- backend/category/categoryRetreival.py
+++ backend/category/categoryRetreival.py
@@ -7,7 +7,10 @@ def searchProducts(catLevel1,catLevel2):
     res=connectDB()
     conn=res[0]
     cur=res[1]
-    cur.execute("select product_id,title,description,image_url,price from product FULL OUTER JOIN category on product.cat_id = category.cat_id where parent_id=%s AND cat_label=%s ",(catLevel1,catLevel2))
+    if(catLevel2=="women" or catLevel2=="men"):
+        cur.execute("select product_id,title,description,image_url,price from product  INNER JOIN category on product.cat_id = category.cat_id where parent_id=%s",(catLevel1))
+    else:
+        cur.execute("select product_id,title,description,image_url,price from product  INNER JOIN category on product.cat_id = category.cat_id where parent_id=%s AND cat_label=%s ",(catLevel1,catLevel2))
     data=cur.fetchall()
     cur.close()
     conn.close()

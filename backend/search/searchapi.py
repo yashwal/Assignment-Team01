090f98872726088cab1ebb5d9e87c208448aed6f
Yashwal S Kanchan <kanchanyashwal@gmail.com>
2023-02-05 16:01:07 +0530

    style(Removing-quotes): Removed and commented a block

--- backend/search/searchapi.py
+++ backend/search/searchapi.py
@@ -8,11 +8,11 @@ def searchProduct(searchQuery,pageNumber,sortKey):
     unbxdApi = f"https://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?q={searchQuery}&rows=9&start={pageNumber}&sort={sortKey}"
     response=requests.get(unbxdApi)
     return response
-
+'''
 def searchSorted(searchQuery,SortKey,pageNumber):
     print(SortKey)
     unbxdApi = f"https://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?q={searchQuery}&sort={SortKey}&rows=9&start={pageNumber}"
     response=requests.get(unbxdApi)
-    return response"""
+    return response
 
-
+'''

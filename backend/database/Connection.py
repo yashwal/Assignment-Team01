65439153a8be0ab61333bce062d32ef077a27827
Yashwal S Kanchan <kanchanyashwal@gmail.com>
2023-02-05 13:04:37 +0530

    fix(Database-Connection): Fixed database connection

--- backend/database/Connection.py
+++ backend/database/Connection.py
@@ -6,6 +6,6 @@ def connectDB():
     psycopg2 module is used for connection.
     '''
     conn = psycopg2.connect(host="assignment-team01-database-1", database="unbxd", user="postgres", password="unbxd")
-    #conn = psycopg2.connect(host="assignment-team01-database-1", database="unbxd", user="postgres", password="unbxd")
+    #conn = psycopg2.connect(host="local-database-1", database="unbxd", user="postgres", password="unbxd")
     cur=conn.cursor()
     return [conn,cur]

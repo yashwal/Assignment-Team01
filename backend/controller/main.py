21916f4be28a3cfc3f71b082a8483bc3a3fcf16f
Yashwal S Kanchan <kanchanyashwal@gmail.com>
2023-02-07 11:48:21 +0530

    refactor(Code-refactor): Deleted uneccessary and reundant codes

--- backend/controller/main.py
+++ backend/controller/main.py
@@ -28,9 +28,9 @@ class fetchProducts(Resource):
     def get(self,productId):
         #productId = request.args.get('uid', default="", type=str)
         data=getProducts(productId)
-        
         return data
 api.add_resource(fetchProducts,"/product/<string:productId>")
+
 
 class categoryTree(Resource):
     @cache.cached(timeout=30, query_string=True)
@@ -45,12 +45,9 @@ api.add_resource(categoryTree,"/categoryTree")
 class upload(Resource):
     def post(self):
         data=request.json
-        
         categoryTable(data)
         productTable(data)
-
         return {"Data Ingestion":"Successfull!!!"}
-
 api.add_resource(upload,"/upload")
 
 
@@ -61,18 +58,10 @@ class productQuery(Resource):
         searchQuery = request.args.get('q', default="", type=str)
         pageNumber = request.args.get('page', default=1, type=int)
         sortKey = request.args.get('sort', default="ftrd", type=str)
-        print(searchQuery,pageNumber,sortKey)
         pageNumber=(pageNumber-1)*9
-        if sortKey=='ftrd':
-            response = searchProduct(searchQuery,pageNumber,"")
-        else:
-            response=searchSorted(searchQuery,sortKey,pageNumber)
-        #response=searchProduct(searchQuery,pageNumber,sortKey)
-        data = json.loads(response.content)
-        products = data['response']['products']
-        productsNumber = data['response']['numberOfProducts']
-        return [productsNumber,products]
-
+        [productsNumber,products] = searchProduct(searchQuery,pageNumber,sortKey)
+        return [products,productsNumber]
+        
 api.add_resource(productQuery,"/product_query")
 
 
@@ -86,7 +75,7 @@ class category(Resource):
         sortKey = request.args.get('sort', default="ftrd", type =str)
         pageNumber=(pageNumber-1)*9
         data=searchProducts(catLevel1,catLevel2)
-        #print(data)
+
         new_data = []
         for product in data:
             new_data.append({"uniqueId":product[0], "Title":product[1], "Description":product[2],"Img_URL":product[3],"price":product[4]})
@@ -102,69 +91,54 @@ class category(Resource):
 
 api.add_resource(category,"/category")
 
-class productSort(Resource):
-    @cache.cached(timeout=30, query_string=True)
-    def get(self,searchQuery,sortKey,pageNumber):
-        pageNumber=max((pageNumber-1)*9,1)
-        if sortKey=='ftrd':
-            response = searchProduct(searchQuery,pageNumber,"")
-        else:
-            response=searchSorted(searchQuery,sortKey,pageNumber)
-        data = json.loads(response.content)
-        products = data['response']['products']
-        productsNumber = data['response']['numberOfProducts']
-        return [productsNumber,products]
 
-api.add_resource(productSort,"/product_query/<string:searchQuery>/<string:sortKey>/<int:pageNumber>")
 
-class categorySort(Resource):
-    @cache.cached(timeout=30, query_string=True)
-    def get(self): #page number should also be an argument
-        catLevel1 = request.args.get('cat1', default="", type=str)
-        catLevel2 = request.args.get('cat2', default="", type=str)
-        sortKey = request.args.get('sort', default="ftrd", type =str)
-        pageNumber = request.args.get('page', default=1, type=int)
-        pageNumber=(pageNumber-1)*9
-        print(sortKey)
-        data=searchProducts(catLevel1,catLevel2)
-        type(data)
-        new_data=[]
-        for product in data:
-            new_data.append({"uniqueId":product[0], "Title":product[1].capitalize(), "Description":product[2],"Img_URL":product[3],"price":product[4]})
-        
-        if(sortKey=="price asc"):
-            new_data = sorted(new_data, key=lambda k: float(k['price']))
-        if(sortKey=="price desc"):
-            new_data = sorted(new_data, key=lambda k: float(k['price']), reverse=True)
 
-        sliced_data=new_data[pageNumber:pageNumber+9]
-
-        return [len(new_data),sliced_data]
-
-api.add_resource(categorySort,"/categorySort")
 
 
 if __name__=='__main__':
     app.run(port=7002,debug=True,host = "0.0.0.0")
 
 
-#future work
 
+# class productSort(Resource):
+#     @cache.cached(timeout=30, query_string=True)
+#     def get(self,searchQuery,sortKey,pageNumber):
+#         pageNumber=max((pageNumber-1)*9,1)
+#         if sortKey=='ftrd':
+#             response = searchProduct(searchQuery,pageNumber,"")
+#         else:
+#             response=searchSorted(searchQuery,sortKey,pageNumber)
+#         data = json.loads(response.content)
+#         products = data['response']['products']
+#         productsNumber = data['response']['numberOfProducts']
+#         return [productsNumber,products]
 
-#update the product details if the product already exists in the database and the product_id is valid
-# class updateProduct(Resource):
-#     def put(self):
-#         data=request.json
-#         for i in data:
-#             productId=i.get("uniqueId")
-#             if(productId==None):
-#                 print({"Message":"Product ID is not given"})
-#                 break
-#             if(checkProductID(productId)==0):
-#                 print({"Message":"Product ID is missing in DB"})
-#                 break
-#             updateDB(i.get("title"),i.get("productDescription"),i.get("productImage"),i.get("price"),productId)
-            
-#         return({"Message":"DB Updated","status":200})
+# api.add_resource(productSort,"/product_query/<string:searchQuery>/<string:sortKey>/<int:pageNumber>")
 
-# api.add_resource(updateProduct,"/updateProduct")  
+# class categorySort(Resource):
+#     @cache.cached(timeout=30, query_string=True)
+#     def get(self): #page number should also be an argument
+#         catLevel1 = request.args.get('cat1', default="", type=str)
+#         catLevel2 = request.args.get('cat2', default="", type=str)
+#         sortKey = request.args.get('sort', default="ftrd", type =str)
+#         pageNumber = request.args.get('page', default=1, type=int)
+#         pageNumber=(pageNumber-1)*9
+#         print(sortKey)
+#         data=searchProducts(catLevel1,catLevel2)
+#         type(data)
+#         new_data=[]
+#         for product in data:
+#             new_data.append({"uniqueId":product[0], "Title":product[1].capitalize(), "Description":product[2],"Img_URL":product[3],"price":product[4]})
+        
+#         if(sortKey=="price asc"):
+#             new_data = sorted(new_data, key=lambda k: float(k['price']))
+#         if(sortKey=="price desc"):
+#             new_data = sorted(new_data, key=lambda k: float(k['price']), reverse=True)
+
+#         sliced_data=new_data[pageNumber:pageNumber+9]
+
+#         return [len(new_data),sliced_data]
+
+#     api.add_resource(categorySort,"/categorySort")
+
